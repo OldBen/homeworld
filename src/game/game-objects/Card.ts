3573d19f9b5a4559bd06ec00935e57ee6d4e3730
Krzysztof Buniewicz <krzysztof.buniewicz@gmail.com>
2025-06-05 12:01:29 +0200

    Card and Deck object skeleton

---
+++ src/game/game-objects/Card.ts
@@ -0,0 +1,4 @@
+class Card {
+
+}
+export default Card;
---
+++ src/game/game-objects/Deck.ts
@@ -0,0 +1,27 @@
+import Card from "./Card";
+
+class Deck {
+    StartingCards: number;
+    CardsInDeck: Card[];
+
+    remainingCards(): number {
+        return this.CardsInDeck.length;
+    }
+
+    shuffleDeck(): void {
+        var m = this.CardsInDeck.length, t, i;
+
+        // While there remain elements to shuffle…
+        while (m) {
+
+            // Pick a remaining element…
+            i = Math.floor(Math.random() * m--);
+
+            // And swap it with the current element.
+            t = this.CardsInDeck[m];
+            this.CardsInDeck[m] = this.CardsInDeck[i];
+            this.CardsInDeck[i] = t;
+        }
+    }
+}
+export default Deck;
